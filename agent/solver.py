c18a50bd39ab82119ff7332a68409bc17dfd9f46
alrdebugne <alrdebugne@gmail.com>
2023-06-06 23:00:38 +0200

    Adds forgotten ReLU in FC

--- agent/solver.py
+++ agent/solver.py
@@ -50,6 +50,7 @@ class CategoricalCNN(nn.Module):
         conv_out_size = self._get_conv_out(input_shape)
         self.fc = nn.Sequential(
             nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
             nn.Linear(512, n_actions)
         )
 
