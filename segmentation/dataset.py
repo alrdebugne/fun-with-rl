ef35ec9335b764e71e5ee2f8dbea0720a0d2fad9
alrdebugne <alrdebugne@gmail.com>
2023-06-21 14:04:46 +0200

    Adds support for train & eval

--- segmentation/dataset.py
+++ segmentation/dataset.py
@@ -17,31 +17,51 @@ class SMBDataset(Dataset):
   for the segmentation model
   """
 
-  def __init__(self, folder: pathlib.Path, transform: Optional[Callable] = None):
+  def __init__(self, folder, mode: str, transform: Optional[Callable] = None):
     """
     Args:
-      folder (pathlib): path to folder containing folders 'images' and 'labels'
-      transform (callable, optional): optional transformation to apply to smaple
+      folder (pathlib): path to folder containing folders "images" and "labels"
+      mode (str): either "train" or "eval". Affects how dataset is partitioned
+      transform (callable, optional): optional transformation to apply to sample
     """
+    assert mode in ["train", "eval"], \
+      f"`mode` must be 'train' or 'eval', but was {mode}."
+
     self.folder = folder
+    self.mode = mode
     self.transform = transform
 
+    # Split images into train and validations
+    all_images = sorted(list((self.folder / Path("images")).glob("*.png")))
+    all_labels = sorted(list((self.folder / Path("labels")).glob("*.png")))
+    # ^ sorted needed to align indices in images and labels
+
+    if mode == "train":
+      start, end = 0, int(len(all_images) * 0.8)
+    else:
+      start, end = int(len(all_images) * 0.8), len(all_images)
+
+    self.images = all_images[start : end]
+    self.labels = all_labels[start : end]
+
+
   def __len__(self):
-    return len(list((self.folder / Path("images")).glob("*.png")))
-  
+    return len(self.images)
+
+
   def __getitem__(self, idx: Union[int, torch.Tensor]):
     """
     Returns dictionary with image and label.
-    
+
     Implementation is dependent on the folder structure and naming convention
     for images and labels (here, simply numerals).
     """
     if torch.is_tensor(idx):
       idx = idx.tolist()
-    
-    image_name = self.folder / Path(f"images/{idx}.png")
-    label_name = self.folder / Path(f"labels/{idx}.png")
-    # ^ check this works with torch.Tensor
+
+    image_name = self.images[idx]
+    label_name = self.labels[idx]
+    # TODO: check this works with torch.Tensor
     image = Image.open(image_name).convert("RGB")
     label = Image.open(label_name)
 
@@ -50,26 +70,29 @@ class SMBDataset(Dataset):
       "label": (255 * transforms.ToTensor()(label)).long()
     }
     if self.transform:
-      # TODO: might need different transformation for train and eval
-      sample = self.transform(sample)
-    
+      sample = self.transform(sample, self.mode)
+
     return sample
 
-
-def crop_flip(sample: Dict[str, Image.Image]) -> Dict[str, Image.Image]:
+def crop_flip(sample: Dict[str, Image.Image], mode: str) -> Dict[str, Image.Image]:
   """
-  Applies random cropping and horizontal flips to train images
+  During training, applies random cropping and horizontal flips
+  At evaluation, simply centre-crops images.
   """
-  
-  # Cropping
-  i, j, h, w = transforms.RandomCrop.get_params(sample["image"], (224, 224))
-  sample["image"] = TF.crop(sample["image"], i, j, h, w)
-  sample["label"] = TF.crop(sample["label"], i, j, h, w)
 
-  # Flipping
-  if random.random() > 0.5:
-    sample["image"] = TF.hflip(sample["image"])
-    sample["label"] = TF.hflip(sample["label"])
+  if mode == "train":
+    # cropping
+    i, j, h, w = transforms.RandomCrop.get_params(sample["image"], (224, 224))
+    sample["image"] = TF.crop(sample["image"], i, j, h, w)
+    sample["label"] = TF.crop(sample["label"], i, j, h, w)
+    # flipping
+    if random.random() > 0.5:
+      sample["image"] = TF.hflip(sample["image"])
+      sample["label"] = TF.hflip(sample["label"])
+
+  else:
+    sample["image"] = TF.center_crop(sample["image"], [224, 224])
+    sample["label"]  = TF.center_crop(sample["label"], [224, 224])
 
   return sample
 
