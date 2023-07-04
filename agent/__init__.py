cb8b68c6b1182dd94525739c8ee583b4c43baa79
alrdebugne <alrdebugne@gmail.com>
2023-07-04 21:34:46 +0200

    Corrects import at agent module

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,4 +1,4 @@
+from .solver import CategoricalCNN, CategoricalMLP
 from .ac import ActorCritic
 from .ddqn import DDQNAgent
 from .ppo import PPOAgent
-from .solver import CategoricalCNN, CategoricalMLP
